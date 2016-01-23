cc7bb09e10013fc881590c1b04c5eef042142911
Conner Simmering <connersimmering@gmail.com>
2016-01-23 11:09:36 -0500

    Environment Reactivity

--- client/templates/environments/environment_list.js
+++ client/templates/environments/environment_list.js
@@ -9,6 +9,17 @@ Template.environmentList.events({
    },
    'click .newObs': function(e) {
       e.preventDefault();
+      Router.go('newObservation');
+   },
+   'click .createNewEnvironment': function(e) {
+      e.preventDefault();
       Router.go('environmentCreate');
    }
+});
+
+Template.environmentList.helpers({
+  environment: function() {
+    console.log("Template.environmentList.helpers");
+    return Environments.find({}, {sort: {submitted: -1}});
+  }
 });
