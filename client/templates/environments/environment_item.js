6493bd270cf8429de26fea8a4ef5e200b02ec240
Conner Simmering <connersimmering@gmail.com>
2016-01-23 14:35:33 -0500

    observation-pages-separate-ids

---
+++ client/templates/environments/environment_item.js
@@ -0,0 +1,11 @@
+// Template.environmentItem.rendered=function() {
+//     console.log(this);
+//     this.$('#deleteEnvironement').confirmation();
+// }
+
+ // Template.environmentList.events({
+ //    'click .viewObsItem': function(e) {
+ //      console.log(this);
+ //      console.log(this._id);
+ //    }
+ // });
