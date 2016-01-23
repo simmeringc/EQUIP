1dd5d65b88cdbbbf2c591d80ca1cead5f96759a5
birdman7010 <mansfield8@yahoo.com>
2016-01-23 17:49:22 -0500

    fixed merge conflicts

--- client/templates/environments/environment_item.js
+++ client/templates/environments/environment_item.js
@@ -22,3 +22,27 @@ Template.environmentItem.rendered=function() {
     }
   });
 }
+
+Template.environmentItem.events({
+   'click .deleteEnvironment': function(e) {
+     Session.set('environmentId', this);
+     //console.log('environmentId');
+   }
+ });
+
+ Template.environmentItem.rendered=function() {
+     $('.deleteEnvironment').confirmation({
+       onConfirm : function(){
+         var environmentId = Session.get('environmentId');
+         //console.log(environmentId);
+       Meteor.call('environmentDelete', environmentId, function(error, result) {
+         Router.go('environmentList')
+       });
+       }
+     });
+     // $('.deleteEnvironment').confirmation({
+     //   onCancel : function(){
+     //   console.log("ok");
+     //   }
+     // });
+ }
