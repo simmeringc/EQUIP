e15947a4936e5f5f15cd66b1983eb9a5c193b571
birdman7010 <mansfield8@yahoo.com>
2016-01-24 03:43:20 -0500

    Sign up button

---
+++ client/templates/landing_page/landing_page.js
@@ -0,0 +1,8 @@
+Template.landingPage.events({
+  'click [data-action="signUp"]': function(e) {
+    e.stopPropagation();
+    $('#login-dropdown-list').addClass('open');
+    $('#login-username-or-email').focus();
+    console.log("sweeet");
+  }
+});
