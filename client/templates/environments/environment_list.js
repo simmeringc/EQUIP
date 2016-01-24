c63f69fc6655489d58d1a018f4cefe142b8e04ab
Conner Simmering <connersimmering@gmail.com>
2016-01-23 23:38:40 -0500

    Modal creators env obs

--- client/templates/environments/environment_list.js
+++ client/templates/environments/environment_list.js
@@ -1,29 +1,29 @@
-Template.environmentList.helpers({
+Template.observationList.helpers({
   environment: function() {
     return Environments.find({}, {sort: {submitted: -1}});
   }
 });
 
-Template.environmentList.events({
-   'click .createNewEnvironment': function(e) {
+Template.observationList.events({
+   'click .createNewObservation': function(e) {
     $('createPopup').modal({
       keyboard: true,
       show: true
     });
-
    }
 });
 
-Template.environmentList.events({
+Template.observationList.events({
   'click #saveName': function(e) {
 
     var environment = {
-      nameOfEnvironment: $('#environmentText').val()
+      nameOfEnvironment: $('#nameOfObservation').val()
     };
 
     Meteor.call('environmentInsert', environment, function(error, result) {
       Router.go('environmentList');
     });
+
     $('#createPopup').modal('hide');
   }
 });
--- client/templates/observations/observation_list.js
+++ client/templates/observations/observation_list.js
@@ -3,3 +3,33 @@ Template.observationList.helpers({
     return Environments.find({_id: this._id});
   }
 });
+
+Template.observationList.helpers({
+  environment: function() {
+    return Environments.find({}, {sort: {submitted: -1}});
+  }
+});
+
+Template.observationList.events({
+   'click .createNewObservation': function(e) {
+    $('createPopup').modal({
+      keyboard: true,
+      show: true
+    });
+   }
+});
+
+Template.observationList.events({
+  'click #saveName': function(e) {
+
+    var observation = {
+      nameOfObservation: $('#nameOfObservation').val()
+    };
+
+    Meteor.call('observationInsert', observation, function(error, result) {
+      Router.go('observationList');
+    });
+
+    $('#createPopup').modal('hide');
+  }
+});
