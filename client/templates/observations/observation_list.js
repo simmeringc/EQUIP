5e32a060016a14d5e3ba2c7ad0c04333375948e3
Conner Simmering <connersimmering@gmail.com>
2016-01-24 02:50:55 -0500

    completed rendering observations

--- client/templates/observations/observation_list.js
+++ client/templates/observations/observation_list.js
@@ -1,11 +1,8 @@
 Template.observationList.helpers({
   environment: function() {
     return Environments.find({_id: this._id});
-  }
-});
-
-Template.observationList.helpers({
+  },
   observation: function() {
-    return Observations.find({evironmentId: this._id});
+    return Observations.find({environmentId: this._id}, {sort: {submitted: -1}});
   }
 });
