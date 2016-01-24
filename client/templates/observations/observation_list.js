31fad8cf2852651d0043e555e10d0e9e82ce934e
Conner Simmering <connersimmering@gmail.com>
2016-01-24 00:32:36 -0500

    fixing 4

--- client/templates/observations/observation_list.js
+++ client/templates/observations/observation_list.js
@@ -1,35 +1,5 @@
-Template.observationList.helpers({
-  environment: function() {
-    return Environments.find({_id: this._id});
-  }
-});
-
-Template.observationList.helpers({
-  environment: function() {
-    return Environments.find({}, {sort: {submitted: -1}});
-  }
-});
-
-Template.observationList.events({
-   'click .createNewObservation': function(e) {
-    $('createPopup').modal({
-      keyboard: true,
-      show: true
-    });
-   }
-});
-
-Template.observationList.events({
-  'click #saveName': function(e) {
-
-    var observation = {
-      nameOfObservation: $('#nameOfObservation').val()
-    };
-
-    Meteor.call('observationInsert', observation, function(error, result) {
-      Router.go('observationList');
-    });
-
-    $('#createPopup').modal('hide');
-  }
-});
+// Template.observationList.helpers({
+//   observation: function() {
+//     return Observation.find({evironmentId: this._id});
+//   }
+// });
