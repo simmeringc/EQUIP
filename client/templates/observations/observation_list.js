7997a7c41971fa7f465d3bd89bd0633d311a1576
Conner Simmering <connersimmering@gmail.com>
2016-01-28 17:13:25 -0700

    removed obslist environment helper, this already set with data context

--- client/templates/observations/observation_list.js
+++ client/templates/observations/observation_list.js
@@ -1,7 +1,4 @@
 Template.observationList.helpers({
-  environment: function() {
-    return Environments.find({_id: this._id});
-  },
   observation: function() {
     return Observations.find({environmentId: this._id}, {sort: {submitted: -1}});
   }
