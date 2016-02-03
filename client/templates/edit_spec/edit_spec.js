db658c33e15a2844b5408a9f6074ec2fc02ba747
Justin <justinwayneolson@gmail.com>
2016-02-03 13:48:39 -0500

    added helper functions for drag

--- client/templates/edit_spec/edit_spec.js
+++ client/templates/edit_spec/edit_spec.js
@@ -1,7 +1,69 @@
+Template.editSpec.rendered = function() {
+   interact('.draggable')
+     .draggable({
+       // enable inertial throwing
+       inertia: true,
+       // keep the element within the area of it's parent
+       // enable autoScroll
+       autoScroll: true,
+
+       // call this function on every dragmove event
+       onmove: dragMoveListener,
+       // call this function on every dragend event
+       onend: function (event) {
+         var textEl = event.target.querySelector('p');
+
+         textEl && (textEl.textContent =
+           'moved a distance of '
+           + (Math.sqrt(event.dx * event.dx +
+                        event.dy * event.dy)|0) + 'px');
+       }
+     });
+
+  function dragMoveListener (event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
+
+  // this is used later in the resizing and gesture demos
+  window.dragMoveListener = dragMoveListener;
+}
+
 Template.editSpec.events({
   'click .specDone': function(e) {
     Router.go('observationList', {_envId:Router.current().params._envId});
   },
+
+   'click #save_subjects': function(e) {
+     $.each( $('.subjects'), function(i, subjects) {
+        $('.subject', subjects).each(function() {
+           $(".subject").removeClass("draggable");
+        });
+      })
+     $("#save_subjects").remove();
+     $("#control_bar").append('<button type="button" id=move_subjects class="btn btn-warning">Move Subjects</button>')
+   },
+
+   'click #move_subjects': function(e) {
+     $.each( $('.subjects'), function(i, subjects) {
+        $('.subject', subjects).each(function() {
+           $(".subject").addClass("draggable");
+        });
+      })
+     $("#move_subjects").remove();
+     $("#control_bar").append('<button type="button" id=save_subjects class="btn btn-warning">Save Subjects</button>')
+   },
 
   'click #createNewSubject': function(e) {
    $('#createSubjPopup').modal({
