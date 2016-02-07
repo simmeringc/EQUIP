1f1e04d683ed370af1ebb70fbd5e134ce0299199
Conner Simmering <connersimmering@gmail.com>
2016-02-06 20:14:19 -0700

    sequence work

--- client/templates/observatory/observatory.js
+++ client/templates/observatory/observatory.js
@@ -7,5 +7,32 @@ Template.observatory.helpers({
 Template.observatory.events({
   'click .obsDone': function(e) {
     Router.go('observationList', {_envId:Router.current().params._envId});
-  }
+  },
+
+  //sequence started in subject_item.js
+  'click #saveSequence': function(e) {
+   var subjId = Session.get('subjId');
+   var sequence = {
+     wcdType: $('#wcdType').val(),
+     solicitationMethod: $('#solicitationMethod').val(),
+     waitTime: $('#waitTime').val(),
+     lengthOfTalk: $('#lengthOfTalk').val(),
+     studentTalk: $('#studentTalk').val(),
+     teacherQuestion: $('#teacherQuestion').val(),
+     explicitEvaluation: $('#explicitEvaluation').val(),
+     subjId: subjId,
+     obsId: Router.current().params._envId,
+     obsId: Router.current().params._obsId
+   };
+
+   Meteor.call('sequenceInsert', sequence, function(error, result) {
+     return 0;
+   });
+
+   $('#createSequence').modal('hide');
+  //  $('#subjectName').val('');
+  //  $('#subjectAge').val('');
+  //  $('#subjectGender').val('');
+  //  $('#SubjectRace').val('');
+ }
 });
