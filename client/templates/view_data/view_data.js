ecadd327cf863f148e83ed654f74d5f9c2ea24cf
Justin <justinwayneolson@gmail.com>
2016-03-08 15:07:20 -0500

    partial

--- client/templates/view_data/view_data.js
+++ client/templates/view_data/view_data.js
@@ -1,5 +1,18 @@
 Template.viewData.helpers({
-  environment: function() {
-    return Environments.find({}, {sort: {submitted: -1}});
-  }
+   environment: function() {
+      return Environments.find({}, {sort: {submitted: -1}});
+   },
 });
+
+Template.viewData.helpers({
+   sequences: function() {
+      return Sequences.find();
+   },
+});
+
+Template.viewData.events({
+   'click #graph_button': function(e) {
+      console.log(Sequences.find({"obsId" : "qKgZ8hdEQuZcuRcAQ"},{"wcdType":true}));
+   }
+});
+
