cca69ee36447f218c9ce19a66390a34e4233ac6b
Conner Simmering <connersimmering@gmail.com>
2016-06-03 13:34:48 -0600

    characteristic-modal

--- client/templates/edit_spec/edit_spec.js
+++ client/templates/edit_spec/edit_spec.js
@@ -1,7 +1,8 @@
-/* InteractJS Start */
+
 
 Template.editSpec.rendered = function() {
 
+/* InteractJS Start */
 interact('.draggable')
   .draggable({
     onmove: window.dragMoveListener,
@@ -53,8 +54,8 @@ interact('.draggable')
 
   // this is used later in the resizing and gesture demos
   window.dragMoveListener = dragMoveListener;
+  /* InteractJS End */
 }
-/* InteractJS End */
 
 Template.editSpec.events({
   'click #esGoBack': function(e) {
