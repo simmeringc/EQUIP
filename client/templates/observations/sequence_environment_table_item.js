16bc1c821c63e83b61bd5445504779b4ce1880b9
Conner Simmering <connersimmering@gmail.com>
2016-06-21 17:23:00 -0600

    tables-finished

---
+++ client/templates/observations/sequence_environment_table_item.js
@@ -0,0 +1,23 @@
+Template.sequenceEnvironmentTableItem.rendered = function() {
+  propigateSequenceTable();
+}
+
+function propigateSequenceTable() {
+
+  var envId = Router.current().params._envId
+  var container = document.getElementById("tdCol");
+
+  parametersObj = SequenceParameters.find({'children.envId':envId}).fetch();
+  parameterPairs = parametersObj[0]["children"]["parameterPairs"]
+
+  var split = []
+  for (i=0;i<parameterPairs;i++) {
+    split[i] = parametersObj[0]["children"]["label"+i]
+  }
+
+  $(".ftable_head").append("<td>#</td>");
+  for (i=0;i<split.length;i++) {
+    $(".ftable_head").append("<td>"+split[i]+"</td>");
+  }
+  $(".ftable_head").append("<td></td>");
+}
